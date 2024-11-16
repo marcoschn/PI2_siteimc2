de508697329e3cf932dc7bbf7e964883f2a64dc0
marcoschn <marcoschn@yahoo.com.br>
2024-11-16 16:00:18 -0300

    terceiro commit

--- siteimc2.py
+++ siteimc2.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL, MySQLdb
 
+from data import data_loader
+
 import MySQLdb.cursors
 import re
 
@@ -18,6 +20,13 @@ app.config['MYSQL_PASSWORD'] = 'UnivPi0811!'
 app.config['MYSQL_DB'] = 'univespi2'
 
 mysql = MySQL(app)
+
+#carregando grafico
+data_loader_obj = data_loader.DataLoader()
+DATA_SET_FULL = data_loader_obj.prepare_data_set_full()
+DATA_SET_GROUPED = data_loader_obj.prepare_data_set_grouped()
+#fim
+
 
 @app.route("/")
 def index():
@@ -91,7 +100,8 @@ def perfil():
             DATE_FORMAT(dadosusuario.dtnascimento, '%Y-%m-%d') as dtnasc, 
             usuarios.codusuario as idusuario, 
             usuarios.nome as nome, 
-            usuarios.email as email  
+            usuarios.email as email  ,
+            usuarios.senha as senha
             FROM univespi2.usuarios 
             inner JOIN univespi2.dadosusuario  
             ON usuarios.codusuario = dadosusuario.codusuario 
@@ -106,6 +116,106 @@ def perfil():
     else:
         return redirect(url_for('index'))
 
+@app.route('/atualizaperfil', methods =['GET', 'POST'])
+def atualizaperfil():
+    if session['loggedin'] == True:
+        if request.method == 'POST':
+            usuario= request.form['nomeusuario']
+            usuarioinicial=request.form['usuarioinicial']
+            emailinicial=request.form['emailinicial']
+            email = request.form['emailusuario']
+            nomecompleto = request.form['nomecompleto']
+            dtnascimento = request.form['dtnasc']
+            senha=request.form['senha']
+            senharepita = request.form['senharepita']
+            if usuario != usuarioinicial:
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute('SELECT * FROM usuarios WHERE nome = % s', (usuario,))
+                account = cursor.fetchone()
+                if account:
+                    problemausuario = 1
+                else:
+                    problemausuario = 0
+            else:
+                problemausuario = 0
+
+            if email != emailinicial:
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute('SELECT * FROM usuarios WHERE nome = % s', (usuario,))
+                emailusu = cursor.fetchone()
+                if emailusu:
+                    problemaemail = 1
+                else:
+                    problemaemail = 0
+            else:
+                problemaemail = 0
+
+            if senha != senharepita:
+                problemasenha= 1
+            else:
+                problemasenha= 0
+
+            resultado = problemausuario + problemaemail + problemasenha
+            print("resultado:" + str(resultado))
+            print(usuario)
+            print(usuarioinicial)
+            print(nomecompleto)
+            print(dtnascimento)
+            print(email)
+            print(emailinicial)
+            print(senha)
+            print(senharepita)
+            if resultado == 0:
+                cursorregistros = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                sqlatualizaperfil="UPDATE dadosusuario SET nome = '" + nomecompleto + "', dtnascimento='" + dtnascimento + "' where codusuario=" + str(session['id'])
+                cursorregistros.execute(sqlatualizaperfil)
+                sqlatualizausuario = "UPDATE usuarios SET nome = '" + usuario + "', email='" + email + "', senha='" + senha + "' where codusuario=" + str(session['id'])
+                cursorregistros.execute(sqlatualizausuario)
+                mysql.connection.commit()
+                flash('Perfil atualizado.')
+                return redirect(url_for('home'))
+            else:
+                if problemausuario==1:
+                    flash('Esse usuario já existe. Digite outro nome de usuário')
+                if problemasenha==1:
+                    flash('As senhas não são iguais. Por favor, confirme corretamente sua senha.')
+                if problemaemail==1:
+                    flash('Esse email já está sendo utilizado por alguém. Por favor, escolha outro.')
+                #return redirect(url_for('home'))
+                return redirect(request.referrer)
+
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/grafico', methods=['GET', 'POST'])
+def grafico():
+    """
+    the main route rendering index.html
+    :return:
+    """
+
+    labels = [x[0] for x in DATA_SET_GROUPED]
+    values_confirmed = [x[1] for x in DATA_SET_GROUPED]
+    values_deaths = [x[2] for x in DATA_SET_GROUPED]
+    values_recovered = [x[3] for x in DATA_SET_GROUPED]
+
+    return render_template('grafico.html', template_labels=labels,
+                           template_values_confirmed=values_confirmed,
+                           template_values_deaths=values_deaths,
+                           template_values_recovered=values_recovered)
+
+
+
+@app.route('/<string:item>', methods=['GET'])
+def get_item_details(item):
+    """
+    the route for each "drilldown" item details
+    :param item:
+    :return:
+    """
+    filtered_data_set = [x for x in DATA_SET_FULL if x.get('ObservationDate') == item]
+
+    return render_template('details.html', template_data_set=filtered_data_set)
 
 @app.route('/add_registro', methods=['GET', 'POST'])
 def add_registro():
@@ -204,7 +314,7 @@ def registrar():
             cursor.execute('SELECT * FROM usuarios WHERE email = % s', (email, ))
             account = cursor.fetchone()
             if account:
-                msg = 'Esse email já está cadastrado'
+                msg = 'Esse email já está cadastrado. Use outro'
             elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
                 msg = 'Email inválido'
             elif not re.match(r'[A-Za-z0-9]+', username):
@@ -213,6 +323,12 @@ def registrar():
                 msg = 'Por favor, preencha seus dados!'
             else:
                 cursor.execute('INSERT INTO usuarios VALUES (NULL, % s, % s, % s)', (email, password, username, ))
+                cursor.execute('select last_insert_id() from usuarios')
+                idusuario=cursor.lastrowid
+                #print(idusuario)
+                sqlinsertdados="INSERT INTO dadosusuario VALUES (" + str(idusuario) + ", '" + username + "', '1900-01-01')"
+                #print(sqlinsertdados)
+                cursor.execute(sqlinsertdados)
                 mysql.connection.commit()
                 msg = 'Usuário cadastrado com sucesso!'
                 #return render_template('registrar.html', msg=msg)
